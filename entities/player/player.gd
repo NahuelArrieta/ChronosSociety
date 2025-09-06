17983da223431bf13a1e168950e0a59ac60668d0
NahuelArrieta <99760313+NahuelArrieta@users.noreply.github.com>
2025-09-06 11:17:33 -0300

    Second player (#3)
    * add second player basic movement
    
    * add second player

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,14 +2,26 @@ extends CharacterBody2D
 
 @export var speed = 300.0
 @export var jump_velocity = -450.0
+@export var player_number = 1
+
+## Input map
+var left_action: String
+var right_action: String
+var jump_action: String
+
 
 var is_reverting = false
 var _state_history = []
 
 func _ready():
+	left_action = "LEFT_P%d" % player_number
+	right_action = "RIGHT_P%d" % player_number
+	jump_action = "JUMP_P%d" % player_number
+	
 	Global.revert_started.connect(start_reverting)
 	Global.revert_stopped.connect(stop_reverting)
 	add_to_group(Global.TIME_TRAVEL_GROUP)
+
 
 func _physics_process(delta):
 	if is_reverting:
@@ -18,10 +30,10 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += Global.gravity * delta
 	
-	if is_on_floor() and Input.is_action_just_pressed("ui_accept"): # TODO
+	if is_on_floor() and Input.is_action_just_pressed(jump_action):
 		velocity.y = jump_velocity 
 
-	var direction = Input.get_axis("ui_left", "ui_right") # TODO
+	var direction = Input.get_axis(left_action, right_action) 
 	velocity.x = direction * speed
 
 	move_and_slide()
@@ -37,7 +49,9 @@ func record_state():
 	if _state_history.size() > Global._max_history_length:
 		_state_history.pop_back()
 		
-func start_reverting():
+func start_reverting(player):
+	if player == player_number:
+		return
 	is_reverting = true
 
 func stop_reverting():
